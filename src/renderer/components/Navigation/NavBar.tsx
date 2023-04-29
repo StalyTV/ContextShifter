649c6120e9c5f3a98a3944de46b85cf83890ed7f
regloff <remy.egloff@uzh.ch>
2023-04-29 16:00:46 +0200

    Added settings menu

--- src/renderer/components/Navigation/NavBar.tsx
+++ src/renderer/components/Navigation/NavBar.tsx
@@ -4,22 +4,33 @@
  * Written by Remy Egloff <remy.egloff@uzh.ch>, March 2023
  */
 
+import { useState } from 'react';
 import GearIcon from '../Icons/GearIcon';
 import SunMoonIcon from '../Icons/SunMoonIcon';
 import styles from './NavBar.module.scss';
+import Settings from 'renderer/pages/Settings';
 
 export default function NavBar() {
+  const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
+
   const onClickSunMoonIcon = async () => {
     await window.electron.ipcRenderer.invoke('toggle-color-theme');
   };
 
+  const onClickGearIcon = async () => {
+    setIsSettingsOpen(!isSettingsOpen);
+  };
+
   return (
-    <div className={styles.navBar}>
-      <SunMoonIcon
-        className={styles.icon}
-        onClick={() => onClickSunMoonIcon()}
-      />
-      <GearIcon className={styles.icon} />
-    </div>
+    <>
+      <div className={styles.navBar}>
+        <SunMoonIcon
+          className={styles.icon}
+          onClick={() => onClickSunMoonIcon()}
+        />
+        <GearIcon className={styles.icon} onClick={() => onClickGearIcon()} />
+      </div>
+      {isSettingsOpen ? <Settings /> : null}
+    </>
   );
 }
