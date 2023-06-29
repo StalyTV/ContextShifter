9ff8cea4e647967df1be4f4d1c806eb563d95fd0
regloff <remy.egloff@uzh.ch>
2023-06-29 16:40:17 +0200

    Never consider "Windows Shell Experience Host"

--- src/main/entity/ActiveWindow.ts
+++ src/main/entity/ActiveWindow.ts
@@ -47,6 +47,7 @@ export default class ActiveWindow extends BaseEntity {
       .andWhere('active_window.applicationPath IS NOT NULL')
       .andWhere(`active_window.application != 'UserNotificationCenter'`)
       .andWhere(`active_window.application != 'loginwindow'`)
+      .andWhere(`active_window.application != 'Windows Shell Experience Host'`)
       .groupBy('active_window.application')
       .getMany();
     return recentActiveWindows;
